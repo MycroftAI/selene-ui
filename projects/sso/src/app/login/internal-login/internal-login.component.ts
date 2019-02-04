154910e85302aa941d5b6dd3b484146e8117589a
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-04 15:09:28 -0600

    made the error experience better.  authentication failures now take the shape of a snackbar.  pre-authentication failures (i.e. improperly formatted email) now show up on the bottom of the input field

--- projects/sso/src/app/login/internal-login/internal-login.component.ts
+++ projects/sso/src/app/login/internal-login/internal-login.component.ts
@@ -1,43 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
-import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
 
 import { AuthResponse, AppService } from '../../app.service';
 
 const noDelay = 0;
+const tenSeconds = 10000;
+
 @Component({
   selector: 'sso-internal-login',
   templateUrl: './internal-login.component.html',
   styleUrls: ['./internal-login.component.scss']
 })
 export class InternalLoginComponent implements OnInit {
-    public authFailed: boolean;
     public password: string;
-    public passwordIcon = faLock;
-    public username: string;
-    public usernameIcon = faUser;
+    public emailAddress: string;
+    public emailFormControl = new FormControl(null, [Validators.email, Validators.required]);
+    public passwordFormControl = new FormControl(null, [Validators.required]);
 
-  constructor(private authService: AppService) { }
+    constructor(private authService: AppService, private errorSnackbar: MatSnackBar) { }
 
-  ngOnInit() { }
+    ngOnInit() { }
 
-  authorizeUser(): void {
-      this.authService.authorizeInternal(this.username, this.password).subscribe(
-          (response) => { this.onAuthSuccess(response); },
-          (response) => { this.onAuthFailure(response); }
-      );
-  }
+    authorizeUser(): void {
+        this.authService.authorizeInternal(this.emailAddress, this.password).subscribe(
+            (response) => { this.authService.navigateToRedirectURI(noDelay); },
+            (response) => { this.onAuthFailure(response); }
+        );
+    }
 
-  onAuthSuccess(authResponse: AuthResponse): void {
-      this.authFailed = false;
-      this.authService.generateTokenCookies(authResponse);
-      this.authService.navigateToRedirectURI(noDelay);
-  }
-
-  onAuthFailure(authorizeUserResponse): void {
-      if (authorizeUserResponse.status === 401) {
-          this.authFailed = true;
-      }
-  }
-
+    onAuthFailure(authorizeUserResponse): void {
+        if (authorizeUserResponse.status === 401) {
+            this.errorSnackbar.open(
+                'Authentication error, please try again',
+                null,
+                {panelClass: 'mycroft-no-action-snackbar', duration: tenSeconds}
+            );
+        }
+    }
 }
