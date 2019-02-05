9b760769f81aebe1edbfc6617153493b9eca3c34
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-05 11:16:52 -0600

    refactored how navigation items were built

--- projects/globalnav/src/lib/globalnav.component.ts
+++ projects/globalnav/src/lib/globalnav.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 
-import { CookieService } from 'ngx-cookie-service';
 import {
     faBars,
     faLightbulb,
@@ -49,7 +48,6 @@ export class GlobalnavComponent implements OnInit {
         this.isLoggedIn = setLoginStatus();
         this.getUser();
         this.buildNavigationItems();
-        this.buildSignInNav();
     }
 
     getUser() {
@@ -72,73 +70,17 @@ export class GlobalnavComponent implements OnInit {
         }
     }
 
-    buildNavigationItems(): void {
-        const aboutMycroftNav: PrimaryNavItem = {
-            children: [
-                {text: 'Team', url: this.mycroftUrls.wordpress + '/team'},
-                {text: 'Careers', url: this.mycroftUrls.wordpress + '/careers'}
-            ],
-            icon: faRobot,
-            text: 'About Mycroft'
-        };
-        const blogNav: PrimaryNavItem = {
-            icon: faRss,
-            text: 'Blog',
-            url: this.mycroftUrls.wordpress + '/blog'
-        };
-        const communityNav: PrimaryNavItem = {
-            children: [
-                {text: 'Chat', url: this.mycroftUrls.chat},
-                {text: 'Forum', url: this.mycroftUrls.forum}
-            ],
-            icon: faUsers,
-            text: 'Community'
-        };
-        const contributeNav: PrimaryNavItem = {
-            children: [
-                {text: 'Source Code', url: 'https://github.com/MycroftAI'},
-                {text: 'Translate', url: this.mycroftUrls.translate},
-                {text: 'Wake Word', url: this.mycroftUrls.account + '/#/precise'},
-                {text: 'Speech to Text', url: this.mycroftUrls.account + '/#/deepspeech'},
-                {text: 'Text to Speech', url: this.mycroftUrls.mimic}
-            ],
-            icon: faLightbulb,
-            text: 'Contribute'
-        };
-        const getStartedNav: PrimaryNavItem = {
-            children: [
-                {text: 'Get Mycroft', url: this.mycroftUrls.wordpress + '/download'},
-                {text: 'Documentation', url: this.mycroftUrls.wordpress + '/documentation'}
-            ],
-            icon: faRocket,
-            text: 'Get Started'
-        };
-        const marketplaceNav: PrimaryNavItem = {
-            children: [
-                {text: 'Skills', url: this.mycroftUrls.marketplace + '/skills'},
-                {text: 'Hardware', url: this.mycroftUrls.wordpress + '/shop'}
-            ],
-            icon: faStore,
-            text: 'Marketplace'
-        };
-        const accountNav: PrimaryNavItem = {
-            children: [
-                {text: 'Devices', url: this.mycroftUrls.account + '/device'},
-                {text: 'Skills', url: this.mycroftUrls.account + '/skill'},
-                {text: 'Profile', url: this.mycroftUrls.account + '/profile'},
-            ],
-            icon: faUserCircle,
-            text: 'My Account',
-        };
 
+    buildNavigationItems(): void {
         this.navigationItems = [
-            aboutMycroftNav,
-            getStartedNav,
-            blogNav,
-            communityNav,
-            contributeNav,
-            marketplaceNav,
-            accountNav
+            this.defineAboutNav(),
+            this.defineGetStartedNav(),
+            this.defineBlogNav(),
+            this.defineForumNav(),
+            this.defineContributeNav(),
+            this.defineMarketNav(),
+            this.defineAccountNav(),
+            this.defineAuthenticationNav()
         ];
 
         this.footerItems = [
@@ -149,7 +91,96 @@ export class GlobalnavComponent implements OnInit {
         ];
     }
 
-    buildSignInNav() {
+    private defineAboutNav(): PrimaryNavItem {
+        return {
+            children: [
+                {text: 'Team', url: this.mycroftUrls.wordpress + '/team'},
+                {text: 'Careers', url: this.mycroftUrls.wordpress + '/careers'}
+            ],
+            icon: faRobot,
+            text: 'About Mycroft'
+        };
+    }
+
+    private defineBlogNav(): PrimaryNavItem {
+        return {
+            icon: faRss,
+            text: 'Blog',
+            url: this.mycroftUrls.wordpress + '/blog'
+        };
+    }
+
+    private defineForumNav(): PrimaryNavItem {
+        return {
+            children: [
+                {text: 'Chat', url: this.mycroftUrls.chat},
+                {text: 'Forum', url: this.mycroftUrls.forum}
+            ],
+            icon: faUsers,
+            text: 'Community'
+        };
+    }
+
+    private defineContributeNav(): PrimaryNavItem {
+        return {
+            children: [
+                {text: 'Source Code', url: 'https://github.com/MycroftAI'},
+                {text: 'Translate', url: this.mycroftUrls.translate},
+                {text: 'Wake Word', url: this.mycroftUrls.account + '/#/precise'},
+                {text: 'Speech to Text', url: this.mycroftUrls.account + '/#/deepspeech'},
+                {text: 'Text to Speech', url: this.mycroftUrls.mimic}
+            ],
+            icon: faLightbulb,
+            text: 'Contribute'
+        };
+    }
+
+    private defineGetStartedNav(): PrimaryNavItem {
+        return {
+            children: [
+                {text: 'Get Mycroft', url: this.mycroftUrls.wordpress + '/download'},
+                {text: 'Documentation', url: this.mycroftUrls.wordpress + '/documentation'}
+            ],
+            icon: faRocket,
+            text: 'Get Started'
+        };
+    }
+
+    private defineMarketNav(): PrimaryNavItem {
+        return {
+            children: [
+                {text: 'Skills', url: this.mycroftUrls.marketplace + '/skills'},
+                {text: 'Hardware', url: this.mycroftUrls.wordpress + '/shop'}
+            ],
+            icon: faStore,
+            text: 'Marketplace'
+        };
+    }
+
+    private defineAccountNav(): PrimaryNavItem {
+        let deviceUrl = this.mycroftUrls.account + '/device';
+        let skillUrl = this.mycroftUrls.account + '/skill';
+        let profileUrl = this.mycroftUrls.account + '/profile';
+        const singleSignOnUrl = this.mycroftUrls.singleSignOn + '/login?redirect=';
+
+        if (!this.isLoggedIn) {
+            deviceUrl = singleSignOnUrl + deviceUrl;
+            skillUrl = singleSignOnUrl + skillUrl;
+            profileUrl = singleSignOnUrl + profileUrl;
+        }
+        return {
+            children: [
+                {text: 'Devices', url: deviceUrl},
+                {text: 'Skills', url: skillUrl},
+                {text: 'Profile', url: profileUrl},
+            ],
+            icon: faUserCircle,
+            text: 'My Account',
+        };
+
+    }
+
+    private defineAuthenticationNav(): PrimaryNavItem {
         let authenticateNav: PrimaryNavItem = {
             icon: faSignInAlt,
             text: 'Sign In',
@@ -164,6 +195,6 @@ export class GlobalnavComponent implements OnInit {
             };
         }
 
-        this.navigationItems.push(authenticateNav);
+        return authenticateNav;
     }
 }
