1627c4e04f47e5bf8aa3e990a5f05a5dbadc7269
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-11 13:44:49 -0600

    added create account and shared modules

--- projects/account/src/app/app.module.ts
+++ projects/account/src/app/app.module.ts
@@ -5,26 +5,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppService } from './app.service';
+import { CreateAccountModule } from './create-account/create-account.module';
 import { GlobalnavModule } from 'globalnav';
 import { PageNotFoundModule } from 'page-not-found';
 import { DeviceModule } from './device/device.module';
 import { ProfileModule } from './profile/profile.module';
+import { SharedModule } from 'shared';
 
 @NgModule(
     {
-        // declarations: [ AppComponent, PageNotFoundComponent ],
         declarations: [ AppComponent ],
         imports: [
             BrowserModule,
             BrowserAnimationsModule,
+            CreateAccountModule,
             GlobalnavModule,
             HttpClientModule,
             DeviceModule,
             PageNotFoundModule,
             ProfileModule,
+            SharedModule,
             AppRoutingModule
         ],
-        providers: [ ],
+        providers: [ AppService ],
         bootstrap: [ AppComponent ]
     }
 )
