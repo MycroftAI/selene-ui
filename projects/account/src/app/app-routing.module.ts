786b5863e0ed0aa8b77585d94e657cec84369431
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-11 13:40:31 -0600

    added route to create account

--- projects/account/src/app/app-routing.module.ts
+++ projects/account/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CreateAccountComponent } from './create-account/create-account.component';
 import { DeviceComponent } from './device/device.component';
 import { PageNotFoundComponent } from 'page-not-found';
 import { ProfileComponent } from './profile/profile.component';
 // import { SkillComponent } from './skill/skill.component';
 
 const routes: Routes = [
+    { path: 'create-account', component: CreateAccountComponent },
     { path: 'device', component: DeviceComponent },
     { path: 'profile', component: ProfileComponent },
     // { path: 'skill', component: ProfileComponent },
