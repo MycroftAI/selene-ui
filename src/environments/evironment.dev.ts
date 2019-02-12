d6a820664327952b58454414aaab7b2a30862f40
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-12 11:06:41 -0600

    testing having environment files at the workspace level

---
+++ src/environments/evironment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    mycroftUrls: {
+        chat: 'https://chat.mycroft.ai',
+        forum: 'https://community.mycroft.ai',
+        singleSignOn: 'http://sso.mycroft.test',
+        account: 'https://account.mycroft.test',
+        marketplace: 'http://market.mycroft.test',
+        mimic: 'http://mimic.mycroft.ai',
+        translate: 'https://translate-test.mycroft.ai',
+        wordpress: 'https://test.mycroft.ai'
+    }
+};
