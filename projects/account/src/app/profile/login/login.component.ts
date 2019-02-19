f2c5e1af14f678adda2154bad3cac153a7f0ff64
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-19 00:04:15 -0600

    removed password field and get account info as an input

--- projects/account/src/app/profile/login/login.component.ts
+++ projects/account/src/app/profile/login/login.component.ts
@@ -1,38 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import { faGithub, IconDefinition } from '@fortawesome/free-brands-svg-icons';
-
-export interface ToggleLabel {
-    label: string;
-    image?: string;
-    icon?: IconDefinition;
-}
+import { Account } from '../profile.service';
 
 @Component({
-  selector: 'account-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'account-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-    public socialPlatforms: ToggleLabel[];
+export class LoginComponent {
+    @Input() account: Account;
 
-  constructor() { }
-
-  ngOnInit() {
-      const githubLabel: ToggleLabel = {
-          label: 'GitHub',
-          icon: faGithub
-      };
-      const googleLabel: ToggleLabel = {
-          label: 'Google',
-          image: '../../assets/google-logo.png'
-      };
-      const facebookLabel: ToggleLabel = {
-          label: 'Facebook',
-          image: '../../assets/facebook_logo.png'
-      };
-
-      this.socialPlatforms = [googleLabel, facebookLabel, githubLabel];
-  }
-
+    constructor() { }
 }
