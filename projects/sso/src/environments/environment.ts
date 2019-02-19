d6037167483fec064b8fa5a3ccbc3ee8112ee549
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-19 15:24:57 -0600

    copied from account project

--- projects/sso/src/environments/environment.ts
+++ projects/sso/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    mycroftUrls: {
+        chat: 'https://chat.mycroft.ai',
+        forum: 'https://community.mycroft.ai',
+        singleSignOn: 'http://sso.mycroft.test',
+        account: 'https://account.mycroft.test',
+        marketplace: 'http://market.mycroft.test',
+        mimic: 'http://mimic.mycroft.ai',
+        translate: 'https://translate-test.mycroft.ai',
+        wordpress: 'https://test.mycroft.ai'
+    }
 };
 
 /*
