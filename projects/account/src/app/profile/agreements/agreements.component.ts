f17ae8eda9660487adcf5d22e1d2af9cdecf1c7f
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-21 19:13:33 -0600

    refactored ui to no longer use expansion panel and replaced hardocding with data from API

--- projects/account/src/app/profile/agreements/agreements.component.ts
+++ projects/account/src/app/profile/agreements/agreements.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Account } from '../profile.service';
 
 @Component({
-  selector: 'account-agreements',
-  templateUrl: './agreements.component.html',
-  styleUrls: ['./agreements.component.scss']
+    selector: 'account-agreements',
+    templateUrl: './agreements.component.html',
+    styleUrls: ['./agreements.component.scss']
 })
 export class AgreementsComponent implements OnInit {
+    @Input() account: Account;
+    // public termsOfUseAccepted: string;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
