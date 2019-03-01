f98f7cfa21340a0187c9db2106757a3f6a3bae04
Chris Veilleux <veilleux.chris@gmail.com>
2019-02-28 22:09:43 -0600

    removed account retrieval logic

--- projects/account/src/app/app.component.ts
+++ projects/account/src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { environment } from '../environments/environment';
-
 
 @Component({
   selector: 'account-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     public environment = environment;
-    title = 'account';
+    title = 'Account';
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 }
