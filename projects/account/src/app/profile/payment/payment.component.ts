11ac617597e1dee1d7c0f05622dad5d56796532d
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-12 10:56:14 -0500

    new bottom sheet for payment info

---
+++ projects/account/src/app/profile/payment/payment.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+    MatBottomSheetRef,
+    MatDialog,
+    MatDialogRef,
+    MatSnackBar
+} from '@angular/material';
+
+import { ElementOptions, StripeCardComponent, StripeService } from 'ngx-stripe';
+
+import { ProfileService } from '../profile.service';
+import { VerifyCardDialogComponent } from './verify-card-dialog.component';
+
+const twoSeconds = 2000;
+
+
+@Component({
+    selector: 'account-payment',
+    templateUrl: './payment.component.html',
+    styleUrls: ['./payment.component.scss']
+})
+export class PaymentComponent implements OnInit {
+    @ViewChild(StripeCardComponent) card: StripeCardComponent;
+    public cardOptions: ElementOptions = {
+        style: {
+            base: {
+                iconColor: '#22a7f0',
+                color: '#2c3e50',
+                '::placeholder': {
+                    color: '#969fa8'
+                }
+            }
+        }
+    };
+    private membershipType: string;
+    private dialogRef: MatDialogRef<VerifyCardDialogComponent>;
+
+    constructor(
+        private bottomSheetRef: MatBottomSheetRef<PaymentComponent>,
+        private paymentSnackbar: MatSnackBar,
+        private profileService: ProfileService,
+        private stripeService: StripeService,
+        public verifyCardDialog: MatDialog
+
+    ) {
+    }
+
+    ngOnInit() {
+    }
+
+    submitPayment() {
+        this.openDialog();
+        this.stripeService.createToken(this.card.getCard(), {}).subscribe(
+            result => {
+                if (result.token) {
+                    this.updateAccount(result.token.id);
+                } else if (result.error) {
+                    this.showStripeError(result.error.message);
+                }
+            }
+        );
+    }
+
+    openDialog(): void {
+        this.dialogRef = this.verifyCardDialog.open(
+            VerifyCardDialogComponent,
+            {width: '250px'}
+        );
+    }
+
+    updateAccount(stripeToken: string) {
+        const newMembership = {
+            support: {
+                membership: this.membershipType,
+                paymentMethod: 'Stripe',
+                paymentToken: stripeToken
+            }
+        };
+        this.profileService.updateAccount(newMembership).subscribe(
+            () => { this.showStripeSuccess(); }
+        );
+    }
+
+    showStripeSuccess() {
+        this.dialogRef.close();
+        const paymentSnackbarRef = this.paymentSnackbar.open(
+            'Card verification successful',
+            null,
+            {panelClass: 'mycroft-no-action-snackbar', duration: twoSeconds}
+        );
+        paymentSnackbarRef.afterDismissed().subscribe(
+            () => { this.bottomSheetRef.dismiss(); }
+        );
+    }
+
+    showStripeError(errorMessage: string) {
+        this.dialogRef.close();
+        this.paymentSnackbar.open(
+            errorMessage,
+            null,
+            {panelClass: 'mycroft-no-action-snackbar', duration: twoSeconds}
+        );
+    }
+
+    onCancel() {
+        this.bottomSheetRef.dismiss('cancel');
+    }
+}
