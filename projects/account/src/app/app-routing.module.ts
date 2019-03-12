11cf24dc14883159e11c9dde50b40920b6ae93c0
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-12 10:44:55 -0500

    moved feature module routes to their respective modules

--- projects/account/src/app/app-routing.module.ts
+++ projects/account/src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CreateAccountComponent } from './create-account/create-account.component';
-import { DeviceComponent } from './device/device.component';
 import { PageNotFoundComponent } from 'page-not-found';
-import { ProfileComponent } from './profile/profile.component';
-import { SkillComponent } from './skill/skill.component';
 
 const routes: Routes = [
-    { path: 'create-account', component: CreateAccountComponent },
-    { path: 'device', component: DeviceComponent },
-    { path: 'profile', component: ProfileComponent },
-    { path: 'skill', component: SkillComponent },
     { path: '', redirectTo: '/profile', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-    imports: [ RouterModule.forRoot(routes) ],
+    imports: [
+        RouterModule.forRoot(
+            routes,
+            {
+                anchorScrolling: 'enabled',
+                scrollPositionRestoration: 'enabled'
+            }
+        )
+    ],
     exports: [ RouterModule ]
 })
 export class AppRoutingModule {
