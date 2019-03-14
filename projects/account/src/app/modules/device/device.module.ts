9a23ef2e3e62139bb8dd19850143da58b39d462b
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-13 23:50:13 -0500

    removed grouping component as it will not be implemented in initial release

--- projects/account/src/app/modules/device/device.module.ts
+++ projects/account/src/app/modules/device/device.module.ts
@@ -25,14 +25,13 @@ import { DeviceListComponent } from './device-list/device-list.component';
 import { DeviceRoutingModule } from './device-routing.module';
 import { DeviceService } from '../../core/http/device.service';
 import { GeographyComponent } from './attribute/geography/geography.component';
-import { GroupComponent } from './attribute/group/group.component';
 import { PlacementComponent } from './attribute/placement/placement.component';
 import { RemoveComponent } from './remove/remove.component';
 import { VoiceComponent } from './attribute/voice/voice.component';
 import { PreferencesComponent } from './preferences/preferences.component';
 import { WakeWordComponent } from './attribute/wake-word/wake-word.component';
-import { AddComponent } from './add/add.component';
-import { PairingComponent } from './add/pairing/pairing.component';
+import { DeviceAddComponent } from './device-add/device-add.component';
+import { PairingComponent } from './device-add/pairing/pairing.component';
 
 @NgModule({
     declarations: [
@@ -40,17 +39,16 @@ import { PairingComponent } from './add/pairing/pairing.component';
         DeviceComponent,
         DeviceListComponent,
         GeographyComponent,
-        GroupComponent,
         PlacementComponent,
         RemoveComponent,
         VoiceComponent,
         PreferencesComponent,
         WakeWordComponent,
-        AddComponent,
+        DeviceAddComponent,
         PairingComponent,
     ],
     entryComponents: [
-        AddComponent,
+        DeviceAddComponent,
         RemoveComponent
     ],
     imports: [
