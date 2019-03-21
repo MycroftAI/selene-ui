d1b062f276b45d7083b202c9711936cda4145fbd
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-20 22:41:51 -0500

    moved device edit components since they are no longer reused

--- projects/account/src/app/shared/shared.module.ts
+++ projects/account/src/app/shared/shared.module.ts
@@ -9,29 +9,17 @@ import {
     MatInputModule
 } from '@angular/material';
 
-import { CityInputComponent } from './components/city-input/city-input.component';
-import { CountryInputComponent } from './components/country-input/country-input.component';
 import { DisplayFieldComponent } from './components/display-field/display-field.component';
 import { OptionButtonsComponent } from './components/option-buttons/option-buttons.component';
-import { RegionInputComponent } from './components/region-input/region-input.component';
-import { TimezoneInputComponent } from './components/timezone-input/timezone-input.component';
 
 @NgModule({
     declarations: [
-        CityInputComponent,
-        CountryInputComponent,
         DisplayFieldComponent,
         OptionButtonsComponent,
-        RegionInputComponent,
-        TimezoneInputComponent
     ],
     exports: [
-        CityInputComponent,
-        CountryInputComponent,
         DisplayFieldComponent,
         OptionButtonsComponent,
-        RegionInputComponent,
-        TimezoneInputComponent
     ],
     imports: [
         CommonModule,
