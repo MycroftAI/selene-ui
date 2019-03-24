c7d2afcf6906aa9f7e9058b93ec1760b2c1698c9
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-24 15:25:01 -0500

    alphabetized imports, cuz that's how I roll

--- projects/account/src/app/modules/device/device.module.ts
+++ projects/account/src/app/modules/device/device.module.ts
@@ -18,38 +18,38 @@ import {
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import { DeviceRoutingModule } from './device-routing.module';
-import { DeviceService } from '@account/http/device.service';
-import { PreferencesComponent } from './components/view/preferences/preferences.component';
-import { RemoveComponent } from './remove/remove.component';
-import { SharedModule } from '../../shared/shared.module';
-import { DeviceAddComponent } from './pages/device-add/device-add.component';
-import { DeviceListComponent } from './components/view/device-list/device-list.component';
-import { DeviceEditComponent } from './device-edit/device-edit.component';
+import { AddCompleteComponent } from './components/view/add-complete/add-complete.component';
 import { CityInputComponent } from './components/input/city-input/city-input.component';
 import { CountryInputComponent } from './components/input/country-input/country-input.component';
-import { RegionInputComponent } from './components/input/region-input/region-input.component';
-import { TimezoneInputComponent } from './components/input/timezone-input/timezone-input.component';
-import { AddCompleteComponent } from './components/view/add-complete/add-complete.component';
-import { DevicesComponent } from './pages/devices/devices.component';
-import { DeviceInfoComponent } from './components/view/device-info/device-info.component';
 import { DefaultsComponent } from './components/view/defaults/defaults.component';
+import { DeviceAddComponent } from './pages/device-add/device-add.component';
+import { DeviceEditComponent } from './device-edit/device-edit.component';
+import { DeviceInfoComponent } from './components/view/device-info/device-info.component';
+import { DeviceListComponent } from './components/view/device-list/device-list.component';
+import { DeviceRoutingModule } from './device-routing.module';
+import { DevicesComponent } from './pages/devices/devices.component';
+import { DeviceService } from '@account/http/device.service';
+import { PreferencesComponent } from './components/view/preferences/preferences.component';
+import { RegionInputComponent } from './components/input/region-input/region-input.component';
+import { RemoveComponent } from './remove/remove.component';
+import { SharedModule } from '../../shared/shared.module';
+import { TimezoneInputComponent } from './components/input/timezone-input/timezone-input.component';
 
 @NgModule({
     declarations: [
-        DeviceListComponent,
-        RemoveComponent,
-        PreferencesComponent,
-        DeviceAddComponent,
-        DeviceEditComponent,
+        AddCompleteComponent,
         CityInputComponent,
         CountryInputComponent,
-        RegionInputComponent,
-        TimezoneInputComponent,
-        AddCompleteComponent,
-        DevicesComponent,
-        DeviceInfoComponent,
         DefaultsComponent,
+        DeviceAddComponent,
+        DeviceEditComponent,
+        DeviceInfoComponent,
+        DeviceListComponent,
+        DevicesComponent,
+        PreferencesComponent,
+        RegionInputComponent,
+        RemoveComponent,
+        TimezoneInputComponent,
     ],
     entryComponents: [
         DeviceAddComponent,
