54f47fdea883d46ac9e71e6bb8640c7804c3f86b
Chris Veilleux <veilleux.chris@gmail.com>
2019-03-26 19:02:27 -0500

    changed the delete card to show the "are you sure" dialog.

--- projects/account/src/app/modules/profile/components/cards/delete/delete.component.ts
+++ projects/account/src/app/modules/profile/components/cards/delete/delete.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+
+import { DeleteConfirmComponent } from '../../modals/delete-confirm/delete-confirm.component';
 
 @Component({
-  selector: 'account-delete',
-  templateUrl: './delete.component.html',
-  styleUrls: ['./delete.component.scss']
+    selector: 'account-delete',
+    templateUrl: './delete.component.html',
+    styleUrls: ['./delete.component.scss']
 })
 export class DeleteComponent implements OnInit {
     public deleteWarning: string[];
 
-  constructor() { }
+    constructor(public confirmDialog: MatDialog) { }
 
-  ngOnInit() {
-      this.deleteWarning = [
-          'Pressing the button below will delete your account and all data related to it from Mycroft servers.',
-          'It cannot be undone.'
-      ];
-  }
+    ngOnInit() {
+        this.deleteWarning = [
+            'Pressing the button below will delete your account and all data related to it from Mycroft servers.',
+            'It cannot be undone.'
+        ];
+    }
+
+    confirmDelete(): void {
+        this.confirmDialog.open(DeleteConfirmComponent, {minWidth: '320px', maxWidth: '400px'});
+    }
 
 }
