4cfaf1c4564567d4a76d29167360d597c2cb68ea
Chris Veilleux <veilleux.chris@gmail.com>
2019-04-04 16:50:46 -0500

    add display name for known platforms

--- projects/account/src/app/modules/device/components/view/device-info/device-info.component.ts
+++ projects/account/src/app/modules/device/components/view/device-info/device-info.component.ts
@@ -8,10 +8,21 @@ import { Device } from '@account/models/device.model';
 })
 export class DeviceInfoComponent implements OnInit {
     @Input() device: Device;
+    public platforms = {
+        'mycroft_mark_1': {icon: '../assets/mark-1-icon.svg', displayName: 'Mark I'},
+        'mycroft_mark_2': {icon: '../assets/mark-2-icon.svg', displayName: 'Mark II'},
+        'picroft': {icon: '../assets/picroft-icon.svg', displayName: 'Picroft'},
+        'kde': {icon: '../assets/kde-icon.svg', displayName: 'KDE'}
+    };
 
     constructor() { }
 
     ngOnInit() {
   }
+
+    getPlatform(device: Device) {
+        const knownPlatform = this.platforms[device.platform];
+        return knownPlatform ? knownPlatform.displayName : device.platform;
+    }
 
 }
