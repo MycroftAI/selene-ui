0154ad5783b988b51d0c55121ed969a85ccdcf95
Chris Veilleux <veilleux.chris@gmail.com>
2019-04-04 16:51:32 -0500

    add delete device functionality and fix platform names

--- projects/account/src/app/modules/device/components/view/device-list/device-list.component.ts
+++ projects/account/src/app/modules/device/components/view/device-list/device-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSnackBar, MatSnackBarConfig } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
@@ -7,6 +7,8 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { DeviceService } from '@account/http/device.service';
 import { Device } from '@account/models/device.model';
 import { RemoveComponent } from '../../../remove/remove.component';
+
+const fiveSeconds = 5000;
 
 @Component({
     selector: 'account-device-list',
@@ -17,19 +19,24 @@ export class DeviceListComponent implements OnInit {
     public addIcon = faPlus;
     public devices: Device[];
     public platforms = {
-        'mark-one': {icon: '../assets/mark-1-icon.svg', displayName: 'Mark I'},
-        'mark-two': {icon: '../assets/mark-2-icon.svg', displayName: 'Mark II'},
+        'mycroft_mark_1': {icon: '../assets/mark-1-icon.svg', displayName: 'Mark I'},
+        'mycroft_mark_2': {icon: '../assets/mark-2-icon.svg', displayName: 'Mark II'},
         'picroft': {icon: '../assets/picroft-icon.svg', displayName: 'Picroft'},
         'kde': {icon: '../assets/kde-icon.svg', displayName: 'KDE'}
     };
     private selectedDevice: Device;
+    private snackbarConfig = new MatSnackBarConfig();
 
     constructor(
         public dialog: MatDialog,
         private deviceService: DeviceService,
         private route: ActivatedRoute,
-        private router: Router
-    ) { }
+        private router: Router,
+        private snackbar: MatSnackBar
+    ) {
+        this.snackbarConfig.panelClass = 'mycroft-no-action-snackbar';
+        this.snackbarConfig.duration = fiveSeconds;
+    }
 
     ngOnInit() {
         this.route.data.subscribe(
@@ -37,23 +44,38 @@ export class DeviceListComponent implements OnInit {
         );
     }
 
-    onRemovalClick(device: Device) {
+    onRemove(device: Device, index: number) {
         const removalDialogRef = this.dialog.open(RemoveComponent, {data: false});
         this.selectedDevice = device;
         removalDialogRef.afterClosed().subscribe(
             (result) => {
-                if (result) { this.deviceService.deleteDevice(device); }
+                if (result) { this.removeDevice(device, index); }
             }
         );
     }
 
-    onDeviceEdit(device: Device) {
-        this.router.navigate(['/devices', device.id]);
+    removeDevice(device: Device, index: number) {
+        this.deviceService.deleteDevice(device).subscribe(
+            () => {
+                this.devices.splice(index, 1);
+                this.snackbar.open(
+                    'Device removed successfully',
+                    null,
+                    this.snackbarConfig
+                );
+            },
+            () => {
+                this.snackbar.open(
+                    'An error occurred removing the device',
+                    null,
+                    this.snackbarConfig
+                );
+            }
+        );
     }
 
-    getPlatform(device: Device) {
-        const knownPlatform = this.platforms[device.platform];
-        return knownPlatform ? knownPlatform.displayName : device.platform;
+    onEdit(device: Device) {
+        this.router.navigate(['/devices', device.id]);
     }
 
     getDeviceIcon(device: Device) {
