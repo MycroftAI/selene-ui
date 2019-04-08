fdac4d8f0d28ba2b0eddd1d26780d0d8a28035bf
Chris Veilleux <veilleux.chris@gmail.com>
2019-04-08 00:42:49 -0500

    fixed mobile layout for new account setup

--- projects/shared/src/lib/shared.module.ts
+++ projects/shared/src/lib/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule, MatButtonToggleModule } from '@angular/material';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -41,6 +42,7 @@ export function getAuthServiceConfigs() {
     ],
     imports: [
         CommonModule,
+        FlexLayoutModule,
         FontAwesomeModule,
         MatButtonModule,
         MatButtonToggleModule,
