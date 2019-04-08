dc79d0ecaf5968c2658fd3d450dabcde9ab5af28
Chris Veilleux <veilleux.chris@gmail.com>
2019-04-08 12:21:43 -0500

    changed check for authenticated user to validate the refresh token instead of the access token.

--- projects/globalnav/src/lib/globalnav.service.ts
+++ projects/globalnav/src/lib/globalnav.service.ts
@@ -24,10 +24,20 @@ export interface PrimaryNavItem {
   url?: string;
 }
 
+/**
+ * We use two tokens for authentication, and access token and a refresh token.
+ * Using the existence of an access token to determine if a user is logged in
+ * (as this code once did) is problematic because the access token is valid
+ * for a short period of time. Once the access token cookie expires, the cookie
+ * is deleted and not refreshed until the next API call.  This was causing a user
+ * to appear to be logged out when they were in fact logged in.  The refresh token
+ * has a longer term expiration.  When it expires, we will expect the user to log
+ * back in.  So now we use the refresh token to determine the login status.
+ */
 export function setLoginStatus(): boolean {
     const cookies = document.cookie;
-    const seleneTokenExists = cookies.includes('seleneAccess');
-    const seleneTokenEmpty = cookies.includes('seleneAccess=""');
+    const seleneTokenExists = cookies.includes('seleneRefresh');
+    const seleneTokenEmpty = cookies.includes('seleneRefresh=""');
 
     return seleneTokenExists && !seleneTokenEmpty;
 }
