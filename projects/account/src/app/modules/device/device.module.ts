8c554b6b178f41619a58d8ef127cdcdcb8816e10
Chris Veilleux <veilleux.chris@gmail.com>
2019-04-18 13:27:31 -0500

    rearranged some of the imports and removed an unused module

--- projects/account/src/app/modules/device/device.module.ts
+++ projects/account/src/app/modules/device/device.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
     MatAutocompleteModule,
     MatButtonModule,
@@ -10,60 +10,56 @@ import {
     MatExpansionModule,
     MatFormFieldModule,
     MatInputModule,
-    MatRadioModule,
     MatStepperModule,
-    MatTabsModule,
-    MatToolbarModule
+    MatTabsModule
 } from '@angular/material';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AddCompleteComponent } from './components/view/add-complete/add-complete.component';
+import { AddComponent } from './pages/add/add.component';
 import { CityInputComponent } from './components/input/city-input/city-input.component';
 import { CountryInputComponent } from './components/input/country-input/country-input.component';
-import { AddComponent } from './pages/add/add.component';
+import { DefaultsCardComponent } from './components/card/defaults-card/defaults-card.component';
+import { DefaultsComponent } from './pages/defaults/defaults.component';
 import { DeviceComponent } from '@account/app/modules/device/device.component';
+import { DeviceEditCardComponent } from './components/card/device-edit-card/device-edit-card.component';
 import { DeviceEditComponent } from './pages/device-edit/device-edit.component';
 import { DeviceDisplayComponent } from './components/view/device-display/device-display.component';
 import { DeviceListComponent } from './pages/device-list/device-list.component';
 import { DeviceRoutingModule } from './device-routing.module';
 import { DeviceService } from '@account/http/device.service';
+import { GeographyCardComponent } from './components/card/geography-card/geography-card.component';
 import { PreferencesComponent } from './pages/preferences/preferences.component';
 import { RegionInputComponent } from './components/input/region-input/region-input.component';
 import { RemoveComponent } from './remove/remove.component';
 import { SharedModule } from '../../shared/shared.module';
 import { TimezoneInputComponent } from './components/input/timezone-input/timezone-input.component';
 import { PreferencesCardComponent } from './components/card/preferences-card/preferences-card.component';
-import { DefaultsComponent } from './pages/defaults/defaults.component';
-import { DefaultsCardComponent } from './components/card/defaults-card/defaults-card.component';
-import { AutocompleteInputComponent } from './components/input/autocomplete-input/autocomplete-input.component';
-import { GeographyCardComponent } from './components/card/geography-card/geography-card.component';
 import { VoiceCardComponent } from './components/card/voice-card/voice-card.component';
 import { WakeWordCardComponent } from './components/card/wake-word-card/wake-word-card.component';
-import { DeviceEditCardComponent } from './components/card/device-edit-card/device-edit-card.component';
 
 @NgModule({
     declarations: [
         AddCompleteComponent,
+        AddComponent,
         CityInputComponent,
         CountryInputComponent,
-        AddComponent,
+        DefaultsCardComponent,
+        DefaultsComponent,
         DeviceComponent,
+        DeviceEditCardComponent,
         DeviceEditComponent,
         DeviceDisplayComponent,
         DeviceListComponent,
+        GeographyCardComponent,
+        PreferencesCardComponent,
         PreferencesComponent,
         RegionInputComponent,
         RemoveComponent,
         TimezoneInputComponent,
-        PreferencesCardComponent,
-        DefaultsComponent,
-        DefaultsCardComponent,
-        AutocompleteInputComponent,
-        GeographyCardComponent,
         VoiceCardComponent,
         WakeWordCardComponent,
-        DeviceEditCardComponent,
     ],
     entryComponents: [
         DeviceComponent,
@@ -82,10 +78,8 @@ import { DeviceEditCardComponent } from './components/card/device-edit-card/devi
         MatExpansionModule,
         MatFormFieldModule,
         MatInputModule,
-        MatRadioModule,
         MatStepperModule,
         MatTabsModule,
-        MatToolbarModule,
         SharedModule,
         DeviceRoutingModule
     ],
