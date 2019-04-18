36917a6f9436306b3ceda1bb075b125a5520c5a2
Chris Veilleux <veilleux.chris@gmail.com>
2019-04-18 13:54:45 -0500

    added forms module to app module in attempt to fix compile bug

--- projects/account/src/app/app.module.ts
+++ projects/account/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +21,7 @@ import { SkillModule } from './modules/skill/skill.module';
             BrowserModule,
             BrowserAnimationsModule,
             DashboardModule,
+            FormsModule,
             GlobalnavModule,
             HttpClientModule,
             DeviceModule,
