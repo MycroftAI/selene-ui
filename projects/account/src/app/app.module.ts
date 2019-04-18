d24ad3f7baf73dd3f4ffd34f0c8fec3b68bb1c82
Chris Veilleux <veilleux.chris@gmail.com>
2019-04-18 13:49:37 -0500

    added reactive forms module to app module in attempt to fix compile bug

--- projects/account/src/app/app.module.ts
+++ projects/account/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +26,7 @@ import { SkillModule } from './modules/skill/skill.module';
             DeviceModule,
             PageNotFoundModule,
             ProfileModule,
+            ReactiveFormsModule,
             SharedModule,
             SkillModule,
             AppRoutingModule
