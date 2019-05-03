c721fd1004f03ecf1f65976f98b6320096db5ac8
Chris Veilleux <veilleux.chris@gmail.com>
2019-05-03 16:33:43 -0500

    removed dead code

--- projects/account/src/app/core/http/profile.service.ts
+++ projects/account/src/app/core/http/profile.service.ts
@@ -1,24 +1,19 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 
 import { Observable, Subject, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
 import { Account } from '@account/models/account.model';
 import { AccountMembership } from '@account/models/account-membership.model';
-import { Agreement } from '@account/models/agreement.model';
 import { environment } from '../../../environments/environment';
 import { MembershipType } from '@account/models/membership.model';
 
 
 // URLs for the http requests
 const ACCOUNT_URL = '/api/account';
-const AGREEMENT_URL = '/api/agreement/';
 const MEMBERSHIP_URL = '/api/memberships';
-
-const fiveSeconds = 5000;
 
 
 export function storeRedirect() {
@@ -47,29 +42,6 @@ export class ProfileService {
     constructor(private http: HttpClient, private snackBar: MatSnackBar) {
     }
 
-    handle400Error(error: HttpErrorResponse) {
-        if (error.status === 400) {
-            this.snackBar.open(
-                'Account creation failed.',
-                null,
-                {panelClass: 'mycroft-snackbar', duration: fiveSeconds}
-            );
-        }
-        if (error.error instanceof ErrorEvent) {
-            // A client-side or network error occurred. Handle it accordingly.
-            console.error('An error occurred:', error.error.message);
-        } else {
-            // The backend returned an unsuccessful response code.
-            // The response body may contain clues as to what went wrong,
-            console.error(
-                `Backend returned code ${error.status}, ` +
-                `body was: ${error.error}`);
-        }
-        // return an observable with a user-facing error message
-        return throwError(
-        'Something bad happened; please try again later.');
-    }
-
     handleError(error: HttpErrorResponse) {
         if (error.status === 401) {
             console.log(error);
@@ -88,12 +60,6 @@ export class ProfileService {
 
         return throwError(
         'Something bad happened; please try again later.');
-    }
-
-    addAccount(newAcctForm: FormGroup)  {
-        return this.http.post<any>(ACCOUNT_URL, newAcctForm.value).pipe(
-            catchError(this.handle400Error)
-        );
     }
 
     /**
