403985b49b643782bfd575ad4c7c3e14e801e66e
Chris Veilleux <veilleux.chris@gmail.com>
2019-09-15 20:23:28 -0500

    removed debugging log message

--- projects/account/src/app/core/http/skill.service.ts
+++ projects/account/src/app/core/http/skill.service.ts
@@ -34,7 +34,6 @@ export class SkillService {
     }
 
     updateSkillSettings(skillFamily: SkillFamily, skillSettings: SkillSettings[]) {
-        console.log(skillSettings);
         return this.http.put(
             `/api/skills/${skillFamily.familyName}/settings`,
             {skillIds: skillFamily.skillIds, skillSettings: skillSettings}
